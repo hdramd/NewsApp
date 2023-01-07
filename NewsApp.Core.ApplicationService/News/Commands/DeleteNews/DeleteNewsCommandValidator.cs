43dbb065acceb4c67cb0de8c8aed52638c6daf2f
hdramd <heidar.ahmadii89@gmail.com>
2023-01-07 06:02:01 -0800

    Add  delete news validator

---
+++ NewsApp.Core.ApplicationService/News/Commands/DeleteNews/DeleteNewsCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using NewsApp.Core.Contracts.Categories.Queries;
+using NewsApp.Core.Contracts.News.Commands.DeleteNews;
+using Zamin.Extensions.Translations.Abstractions;
+
+namespace NewsApp.Core.ApplicationService.News.Commands.DeleteNews
+{
+	public class DeleteNewsCommandValidator : AbstractValidator<DeleteNewsCommand>
+	{
+		private readonly ICategoryQueryRepository _categoryQueryRepository;
+		public DeleteNewsCommandValidator(ITranslator translator, 
+			ICategoryQueryRepository categoryQueryRepository)
+		{
+			_categoryQueryRepository= categoryQueryRepository;
+
+			RuleFor(x => x.Id)
+				.NotNull().NotEmpty().WithMessage(translator["Required", "Id"])
+				.Must(NotUsed).WithMessage(translator["This category is used in atleast one news."]);
+				
+		}
+
+		private bool NotUsed(long id)
+		{
+			return false;
+		}
+	}
+}
