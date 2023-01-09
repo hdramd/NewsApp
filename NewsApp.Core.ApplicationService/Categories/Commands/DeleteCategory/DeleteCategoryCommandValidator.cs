6f001f0d19966713a163111fd810626ce1e0c3db
hdramd <heidar.ahmadii89@gmail.com>
2023-01-09 17:18:31 +0330

    Add category delete validation

--- NewsApp.Core.ApplicationService/Categories/Commands/DeleteCategory/DeleteCategoryCommandValidator.cs
+++ NewsApp.Core.ApplicationService/Categories/Commands/DeleteCategory/DeleteCategoryCommandValidator.cs
@@ -1,21 +1,31 @@
 ﻿using FluentValidation;
 using NewsApp.Core.Contracts.Categories.Commands.DeleteCategory;
+using NewsApp.Core.Contracts.News.Queries;
+using NewsApp.Core.Contracts.News.Queries.GetByCategoryIdPagedList;
 using Zamin.Extensions.Translations.Abstractions;
 
 namespace NewsApp.Core.ApplicationService.Categories.Commands.DeleteCategory
 {
     public class DeleteCategoryCommandValidator : AbstractValidator<DeleteCategoryCommand>
     {
-        public DeleteCategoryCommandValidator(ITranslator translator)
+        private readonly INewsQueryRepository _newsQueryRepository;
+        public DeleteCategoryCommandValidator(ITranslator translator,
+            INewsQueryRepository newsQueryRepository)
         {
+            _newsQueryRepository = newsQueryRepository;
+
             RuleFor(x => x.Id)
                 .NotNull().NotEmpty().WithMessage(translator["Required", "Id"])
-                .Must(NotUsed).WithMessage(translator["This category used in news."]);
+                .Must(NotUsed).WithMessage(translator["Delete failed. This category used in news."]);
         }
 
         private bool NotUsed(long arg)
         {
-            return false;
+            var pagedData = _newsQueryRepository
+                .GetByCategoryIdPagedListAsync(new GetByCategoryIdPagedListQuery { CategoryId = arg })
+                .GetAwaiter().GetResult();
+
+            return pagedData.QueryResult == null || !pagedData.QueryResult.Any();
         }
     }
 }
